bb058076d42efe3b87d927a63cac73f30e12ffa9
anisheb2007-ebe <anisheb2007@gmail.com>
2026-06-22 22:23:19 +0530

    Add pyramid printing program in Pyramid.c

---
+++ Pyramid.c
@@ -0,0 +1,15 @@
+//Pyramid
+#include <stdio.h>
+int  main(){
+  int n,i,j,s;
+  scanf("%d", &n);
+  for (i=0;i<n;i++){
+    for (s=0;s<n-1-i;s++){
+      printf("  ");
+    }
+    for (j=0;j<(2*i)-1;j++){
+      printf("* ");
+    }
+    printf("\n");
+  }
+}
