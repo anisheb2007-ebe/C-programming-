e63497e542bf9b876738fa5a685076b10220c765
anisheb2007-ebe <anisheb2007@gmail.com>
2026-06-23 08:50:14 +0530

    Add string concatenation function in concatptr.c

---
+++ concatptr.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void concatenate(char *str1, char *str2)
+{
+    while (*str1 != '\0')
+    {
+        str1++;
+    }
+
+    while (*str2 != '\0')
+    {
+        *str1 = *str2;
+        str1++;
+        str2++;
+    }
+
+    *str1 = '\0';
+}
+
+int main()
+{
+    char str1[50] = "Hello ";
+    char str2[] = "World";
+
+    concatenate(str1, str2);
+
+    printf("Concatenated String: %s\n", str1);
+
+    return 0;
+}
